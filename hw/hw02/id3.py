94dda105980493bbb8a4b36fff760195a6f23046
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-04-18 18:26:00 -0700

    HW2: First draft of code for testing.

--- hw/hw02/id3.py
+++ hw/hw02/id3.py
@@ -10,11 +10,14 @@ import sys
 import re
 import math
 # Node class for the decision tree
+from collections import Counter
+
 import node
 
 
 train = None
 varnames = None
+namehash = None
 test = None
 testvarnames = None
 root = None
@@ -25,7 +28,7 @@ def entropy(p):
     # >>>> YOUR CODE GOES HERE <<<<
     # For now, always return "0":
     if p == 0. or p == 1.0: return 0.
-    return p * math.log(p, 2.) + (1 - p) * math.log(1 - p, 2)
+    return -p * math.log(p, 2.) - (1 - p) * math.log(1 - p, 2)
 
 
 # Compute information gain for a particular split, given the counts
@@ -38,16 +41,13 @@ def infogain(py_pxi, pxi, py, total):
     # For now, always return "0":
     h0 = entropy(py / total)
 
+    if isinstance(pxi, int): pxi = [pxi]
+    if isinstance(py_pxi, int): py_pxi = [py_pxi]
+
     h1 = 0
-    for y_i, n_i in [(py_pxi, py), (py - py_pxi, n - py)]:
+    for y_i, n_i in zip(py_pxi, pxi):
         h1 += n_i / total * entropy(y_i / n_i)
-    return h0 / h1
-
-# OTHER SUGGESTED HELPER FUNCTIONS:
-# - collect counts for each variable value with each class label
-# - find the best variable to split on, according to mutual information
-# - partition data based on a given variable
-
+    return h0 - h1
 
 
 # Load data from a file
@@ -57,23 +57,59 @@ def read_data(filename):
     data = []
     header = f.readline().strip()
     varnames = p.split(header)
-    namehash = {}
     for l in f:
         data.append([int(x) for x in p.split(l.strip())])
     return (data, varnames)
 
+
 # Saves the model to a file.  Most of the work here is done in the
 # node class.  This should work as-is with no changes needed.
 def print_model(root, modelfile):
-    f = open(modelfile, 'w+')
-    root.write(f, 0)
+    with open(modelfile, 'w+') as f:
+        root.write(f, 0)
+
 
 # Build tree in a top-down manner, selecting splits until we hit a
 # pure leaf or all splits look bad.
-def build_tree(data, varnames):
+def build_tree(data, varnames, rem_vars=None):
     # >>>> YOUR CODE GOES HERE <<<<
-    # For now, always return a leaf predicting "1":
-    return node.Leaf(varnames, 1)
+    if rem_vars is None: rem_vars = list(range(len(varnames) - 1))
+    # Check for homogeneous leaf
+    y_0 = data[0][-1]
+    if all([y_0 == x[-1] for x in data]):
+        return node.Leaf(varnames, y_0)
+    # No variables remaining
+    if not rem_vars:
+        _build_most_common_val_leaf(varnames, data)
+
+    # Create a split node
+    total, py = len(data), sum(1 for x in data if x[-1] == 1)
+    best_ig, best_var, best_data_x = 0., None, None
+    for var_id in rem_vars:
+        data_x =[[x for x in data if x[var_id] == i] for i in range(2)]
+        py_pxi = [sum(1 for x in data_x[i] if x[-1] == i) for i in range(len(data_x))]
+        pxi = [len(grp) for grp in data_x]
+        if any(l == 0 for l in pxi): continue
+
+        ig = infogain(py_pxi, pxi, py, total)
+
+        if ig > best_ig: best_ig, best_var, best_data_x = ig, var_id, data_x
+
+    # No benefit to more splitting so no information left to gain
+    if best_ig == 0.: return _build_most_common_val_leaf(varnames, data)
+    # Split and create child trees
+    rem_vars = [var_id for var_id in rem_vars if var_id != best_var]
+    return node.Split(varnames, best_var,
+                      build_tree(best_data_x[0], varnames, rem_vars),
+                      build_tree(best_data_x[1], varnames, rem_vars))
+
+
+def _build_most_common_val_leaf(names, data):
+    cntr = Counter([x[-1] for x in data])
+    return node.Leaf(names, cntr.most_common(1)[0][0])
+
+
+    return _build_tree(data, varnames, list(range(len(varnames) - 1)))
 
 
 # "varnames" is a list of names, one for each variable
@@ -96,18 +132,19 @@ def loadAndTrain(trainS,testS,modelS):
     root = build_tree(train, varnames)
     print_model(root, modelfile)
 
+
 def runTest():
-    correct = 0
-    # The position of the class label is the last element in the list.
-    yi = len(test[0]) - 1
-    for x in test:
-        # Classification is done recursively by the node class.
-        # This should work as-is.
-        pred = root.classify(x)
-        if pred == x[yi]:
-            correct += 1
-    acc = float(correct) / len(test)
-    return acc
+    return sum(1 for x in test if x[-1] == root.classify(x)) / len(test)
+    # # The position of the class label is the last element in the list.
+    # correct = 0
+    # yi = len(test[0]) - 1
+    # for x in test:
+    #     # Classification is done recursively by the node class.
+    #     # This should work as-is.
+    #     pred = root.classify(x)
+    #     if pred == x[yi]:
+    #         correct += 1
+    # return acc
 
 
 # Load train and test data.  Learn model.  Report accuracy.

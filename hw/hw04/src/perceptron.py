71aa79ff7e3ca792c63da24c4c06291073108024
Zayd Hammoudeh <zayd.hammoudeh@gmail.com>
2019-05-10 13:40:50 -0700

    HW4: Initial version of perceptron algorithm

--- hw/hw04/src/perceptron.py
+++ hw/hw04/src/perceptron.py
@@ -14,79 +14,102 @@ from math import exp
 
 MAX_ITERS = 100
 
+
 # Load data from a file
 def read_data(filename):
-  f = open(filename, 'r')
-  p = re.compile(',')
-  data = []
-  header = f.readline().strip()
-  varnames = p.split(header)
-  namehash = {}
-  for l in f:
-    example = [int(x) for x in p.split(l.strip())]
-    x = example[0:-1]
-    y = example[-1]
-    # Each example is a tuple containing both x (vector) and y (int)
-    data.append( (x,y) )
-  return (data, varnames)
+    f = open(filename, 'r')
+    p = re.compile(',')
+    data = []
+    header = f.readline().strip()
+    varnames = p.split(header)
+    # namehash = {}
+    for l in f:
+        example = [int(x) for x in p.split(l.strip())]
+        x = example[0:-1]
+        y = example[-1]
+        # Each example is a tuple containing both x (vector) and y (int)
+        data.append((x, y))
+    return data, varnames
+
+
+def dot(a, b):
+    r""" Perform dot product operation between vectors a and b """
+    return sum(a_i * b_i for (a_i, b_i) in zip(a, b))
+
+
+def add(a, b, sign):
+    r""" Perform element-wise addition in form a_i + sign * b_i """
+    return [a_i + sign * b_i for (a_i, b_i) in zip(a, b)]
 
 
 # Learn weights using the perceptron algorithm
 def train_perceptron(data):
-  # Initialize weight vector and bias
-  numvars = len(data[0][0])
-  w = [0.0] * numvars
-  b = 0.0
+    # Initialize weight vector and bias
+    numvars = len(data[0][0])
+    w = [0.0] * numvars
+    b = 0.0
 
-  #
-  # YOUR CODE HERE!
-  #
-
-  return (w,b)
+    #
+    # YOUR CODE HERE!
+    #
+    for _ in range(100):
+        num_wrong = 0
+        for (x, y) in data:
+            pred = predict_perceptron((w, b), x)
+            if pred * y > 0:
+                sign = 1.
+            else:
+                sign = -1.
+                num_wrong += 1
+            w = add(w, x, sign)
+            b += sign
+        if num_wrong == 0:
+            break
+    return w, b
 
 
 # Compute the activation for input x.
 # (NOTE: This should be a real-valued number, not simply +1/-1.)
 def predict_perceptron(model, x):
-  (w,b) = model
+    (w, b) = model
 
-  #
-  # YOUR CODE HERE!
-  #
-
-  return 0.0
+    #
+    # YOUR CODE HERE!
+    #
+    return dot(w, x) + b
 
 
 # Load train and test data.  Learn model.  Report accuracy.
 def main(argv):
-  # Process command line arguments.
-  # (You shouldn't need to change this.)
-  if (len(argv) != 3):
-    print('Usage: perceptron.py <train> <test> <model>')
-    sys.exit(2)
-  (train, varnames) = read_data(argv[0])
-  (test, testvarnames) = read_data(argv[1])
-  modelfile = argv[2]
+    # Process command line arguments.
+    # (You shouldn't need to change this.)
+    if len(argv) != 3:
+        print('Usage: perceptron.py <train> <test> <model>')
+        sys.exit(2)
+    (train, varnames) = read_data(argv[0])
+    (test, testvarnames) = read_data(argv[1])
+    modelfile = argv[2]
 
-  # Train model
-  (w,b) = train_perceptron(train)
+    # Train model
+    (w, b) = train_perceptron(train)
 
-  # Write model file
-  # (You shouldn't need to change this.)
-  f = open(modelfile, "w+")
-  f.write('%f\n' % b)
-  for i in range(len(w)):
-    f.write('%s %f\n' % (varnames[i], w[i]))
+    # Write model file
+    # (You shouldn't need to change this.)
+    f = open(modelfile, "w+")
+    f.write('%f\n' % b)
+    for i in range(len(w)):
+        f.write('%s %f\n' % (varnames[i], w[i]))
 
-  # Make predictions, compute accuracy
-  correct = 0
-  for (x,y) in test:
-    activation = predict_perceptron( (w,b), x )
-    print(activation)
-    if activation * y > 0:
-      correct += 1
-  acc = float(correct)/len(test)
-  print("Accuracy: ",acc)
+    # Make predictions, compute accuracy
+    correct = 0
+    for (x, y) in test:
+        activation = predict_perceptron((w, b), x)
+        print(activation)
+        if activation * y > 0:
+            correct += 1
+    acc = float(correct) / len(test)
+    print("Accuracy: ", acc)
+
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+    main(sys.argv[1:])
